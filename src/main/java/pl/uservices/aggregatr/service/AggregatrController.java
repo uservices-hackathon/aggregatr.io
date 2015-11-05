cec524e474622b57466b71d37345f70987897283
Tomasz Lepiorz <tomasz.lepiorz@hybris.com>
2015-11-05 12:31:55 +0100

    impl

--- src/main/java/pl/uservices/aggregatr/service/AggregatrController.java
+++ src/main/java/pl/uservices/aggregatr/service/AggregatrController.java
@@ -1,15 +1,11 @@
 package pl.uservices.aggregatr.service;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
 import pl.uservices.aggregatr.service.dto.Ingredient;
 import pl.uservices.aggregatr.service.dto.OrderRequest;
 import pl.uservices.aggregatr.service.dto.OrderResponse;
@@ -19,59 +15,71 @@ import pl.uservices.aggregatr.service.external.WaterService;
 import pl.uservices.aggregatr.service.external.YeastService;
 import pl.uservices.aggregatr.service.repository.StockRepository;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 
 @RestController
-public class AggregatrController
-{
+public class AggregatrController {
 
-	@Autowired
-	private StockRepository stockRepository;
+    @Autowired
+    private StockRepository stockRepository;
 
-	@Autowired
-	private HopService hopService;
+    @Autowired
+    private HopService hopService;
 
-	@Autowired
-	private MaltService maltService;
+    @Autowired
+    private MaltService maltService;
 
-	@Autowired
-	private WaterService waterService;
+    @Autowired
+    private WaterService waterService;
 
-	@Autowired
-	private YeastService yeastService;
-
-
-	@RequestMapping(value = "/order", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-	public OrderResponse order(final @RequestBody OrderRequest orderRequest)
-	{
-
-		final Map<Ingredient, Integer> stock = new HashMap<>();
-
-		for (final Ingredient ingredient : orderRequest.getIngredients())
-		{
-			switch (ingredient)
-			{
-
-				case HOP:
-					int orderHop = hopService.orderHop();
-					stock.put(Ingredient.HOP, orderHop);
-					break;
-				case WATER:
-					int orderWater = waterService.orderWater();
-					stock.put(Ingredient.WATER, orderWater);
-					break;
-				case YEAST:
-					int orderYeast = yeastService.orderYeast();
-					stock.put(Ingredient.YEAST, orderYeast);
-					break;
-				case MALT:
-					int orderMalt = maltService.orderMalt();
-					stock.put(Ingredient.MALT, orderMalt);
-					break;
-			}
-		}
+    @Autowired
+    private YeastService yeastService;
 
 
-		return new OrderResponse(stock);
-	}
+    @RequestMapping(value = "/order", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public OrderResponse order(final @RequestBody OrderRequest orderRequest) {
+
+        final Map<Ingredient, Integer> stock = handleIngredientsOrder(orderRequest.getIngredients());
+
+        if (checkThresholds(stock)) {
+
+        }
+
+        return new OrderResponse(stock);
+    }
+
+    boolean checkThresholds(final Map<Ingredient, Integer> stock) {
+        return false;
+    }
+
+    private Map<Ingredient, Integer> handleIngredientsOrder(final List<Ingredient> ingredients) {
+        final Map<Ingredient, Integer> stock = new HashMap<>();
+
+        for (final Ingredient ingredient : ingredients) {
+            switch (ingredient) {
+
+                case HOP:
+                    int orderHop = hopService.orderHop();
+                    stock.put(Ingredient.HOP, stockRepository.modifyHopAmount(orderHop));
+                    break;
+                case WATER:
+                    int orderWater = waterService.orderWater();
+                    stock.put(Ingredient.WATER, stockRepository.modifyWaterAmount(orderWater));
+                    break;
+                case YEAST:
+                    int orderYeast = yeastService.orderYeast();
+                    stock.put(Ingredient.YEAST, stockRepository.modifyYeastAmount(orderYeast));
+                    break;
+                case MALT:
+                    int orderMalt = maltService.orderMalt();
+                    stock.put(Ingredient.MALT, stockRepository.modifyMaltAmount(orderMalt));
+                    break;
+            }
+        }
+        return stock;
+    }
 
 }
