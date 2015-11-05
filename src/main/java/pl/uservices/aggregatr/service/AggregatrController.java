f8e90f12ac6546c0eb231051aa9bbeec58a2c093
Marcin Figiel <marcin.figiel@hybris.com>
2015-11-05 12:44:09 +0100

    Threshold check

--- src/main/java/pl/uservices/aggregatr/service/AggregatrController.java
+++ src/main/java/pl/uservices/aggregatr/service/AggregatrController.java
@@ -1,11 +1,16 @@
 package pl.uservices.aggregatr.service;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
 import pl.uservices.aggregatr.service.dto.Ingredient;
 import pl.uservices.aggregatr.service.dto.OrderRequest;
 import pl.uservices.aggregatr.service.dto.OrderResponse;
@@ -15,71 +20,74 @@ import pl.uservices.aggregatr.service.external.WaterService;
 import pl.uservices.aggregatr.service.external.YeastService;
 import pl.uservices.aggregatr.service.repository.StockRepository;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 
 @RestController
-public class AggregatrController {
+public class AggregatrController
+{
 
-    @Autowired
-    private StockRepository stockRepository;
+	private static final Integer THRESHOLD_VALUE = 1000;
 
-    @Autowired
-    private HopService hopService;
-
-    @Autowired
-    private MaltService maltService;
-
-    @Autowired
-    private WaterService waterService;
-
-    @Autowired
-    private YeastService yeastService;
+	@Autowired
+	private StockRepository stockRepository;
+	@Autowired
+	private HopService hopService;
+	@Autowired
+	private MaltService maltService;
+	@Autowired
+	private WaterService waterService;
+	@Autowired
+	private YeastService yeastService;
 
 
-    @RequestMapping(value = "/order", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public OrderResponse order(final @RequestBody OrderRequest orderRequest) {
+	@RequestMapping(value = "/order", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public OrderResponse order(final @RequestBody OrderRequest orderRequest)
+	{
+		final Map<Ingredient, Integer> stock = handleIngredientsOrder(orderRequest.getIngredients());
+		if (thresholdsExceeded(stock))
+		{
 
-        final Map<Ingredient, Integer> stock = handleIngredientsOrder(orderRequest.getIngredients());
+		}
+		return new OrderResponse(stock);
+	}
 
-        if (checkThresholds(stock)) {
+	private Map<Ingredient, Integer> handleIngredientsOrder(final List<Ingredient> ingredients)
+	{
+		final Map<Ingredient, Integer> stock = new HashMap<>();
+		for (final Ingredient ingredient : ingredients)
+		{
+			switch (ingredient)
+			{
+				case HOP:
+					int orderHop = hopService.orderHop();
+					stock.put(Ingredient.HOP, stockRepository.modifyHopAmount(orderHop));
+					break;
+				case WATER:
+					int orderWater = waterService.orderWater();
+					stock.put(Ingredient.WATER, stockRepository.modifyWaterAmount(orderWater));
+					break;
+				case YEAST:
+					int orderYeast = yeastService.orderYeast();
+					stock.put(Ingredient.YEAST, stockRepository.modifyYeastAmount(orderYeast));
+					break;
+				case MALT:
+					int orderMalt = maltService.orderMalt();
+					stock.put(Ingredient.MALT, stockRepository.modifyMaltAmount(orderMalt));
+					break;
+			}
+		}
+		return stock;
+	}
 
-        }
-
-        return new OrderResponse(stock);
-    }
-
-    boolean checkThresholds(final Map<Ingredient, Integer> stock) {
-        return false;
-    }
-
-    private Map<Ingredient, Integer> handleIngredientsOrder(final List<Ingredient> ingredients) {
-        final Map<Ingredient, Integer> stock = new HashMap<>();
-
-        for (final Ingredient ingredient : ingredients) {
-            switch (ingredient) {
-
-                case HOP:
-                    int orderHop = hopService.orderHop();
-                    stock.put(Ingredient.HOP, stockRepository.modifyHopAmount(orderHop));
-                    break;
-                case WATER:
-                    int orderWater = waterService.orderWater();
-                    stock.put(Ingredient.WATER, stockRepository.modifyWaterAmount(orderWater));
-                    break;
-                case YEAST:
-                    int orderYeast = yeastService.orderYeast();
-                    stock.put(Ingredient.YEAST, stockRepository.modifyYeastAmount(orderYeast));
-                    break;
-                case MALT:
-                    int orderMalt = maltService.orderMalt();
-                    stock.put(Ingredient.MALT, stockRepository.modifyMaltAmount(orderMalt));
-                    break;
-            }
-        }
-        return stock;
-    }
+	private boolean thresholdsExceeded(final Map<Ingredient, Integer> stock)
+	{
+		for (final Integer stockValue : stock.values())
+		{
+			if (stockValue < THRESHOLD_VALUE)
+			{
+				return false;
+			}
+		}
+		return true;
+	}
 
 }
