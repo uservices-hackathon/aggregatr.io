b1d6ad0b4e4f39030f0aeda1f7de1234d5949ba1
Marcin Figiel <marcin.figiel@hybris.com>
2015-11-05 12:38:32 +0100

    Fix request dto

--- src/main/java/pl/uservices/aggregatr/service/dto/OrderRequest.java
+++ src/main/java/pl/uservices/aggregatr/service/dto/OrderRequest.java
@@ -6,15 +6,17 @@ import java.util.List;
 public class OrderRequest
 {
 
-	private final List<Ingredient> ingredients;
+	private List<Ingredient> ingredients;
 
-	public OrderRequest(final List<Ingredient> ingredients)
-	{
-		this.ingredients = ingredients;
-	}
 
 	public List<Ingredient> getIngredients()
 	{
 		return ingredients;
 	}
+
+	public void setIngredients(final List<Ingredient> ingredients)
+	{
+		this.ingredients = ingredients;
+	}
+
 }
